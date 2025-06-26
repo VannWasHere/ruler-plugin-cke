a14de32f8ee1470fa920f0d071a06bb0ce1a5a7c
VannWasHere <ateen1203@gmail.com>
2025-06-26 10:53:40 +0700

    Update CKEditor premium features version to 45.2.1 in package.json and standardize import statements in App.vue for improved consistency and readability.

--- src/App.vue
+++ src/App.vue
@@ -28,9 +28,9 @@ import { computed, ref, onMounted, watchEffect, useTemplateRef } from "vue";
 import { Ckeditor } from "@ckeditor/ckeditor5-vue";
 import "../public/ckeditor5.css";
 import "../public/ckeditor5-premium.css";
-import "../public/document-style.css"
-import "../public/styledocs.css"
-import "../public/my_custom_ck_editor.css"
+import "../public/document-style.css";
+import "../public/styledocs.css";
+import "../public/my_custom_ck_editor.css";
 
 import {
   ClassicEditor,
@@ -277,21 +277,22 @@ const config = computed(() => {
         "../public/fonts.css",
         "../public/my_custom_ck_editor.css",
         "../public/ckeditor5-premium.css",
-        '../public/style_document.css',
+        "../public/style_document.css",
         "../public/ckeditor5.css",
       ],
       fileName: "export-pdf-demo.pdf",
       converterOptions: {
-        format: 'A4',
-        margin_top: '20mm',
-        margin_bottom: '20mm',
-        margin_right: '12mm',
-        margin_left: '12mm',
-        page_orientation: 'portrait',
+        format: "A4",
+        margin_top: "20mm",
+        margin_bottom: "20mm",
+        margin_right: "12mm",
+        margin_left: "12mm",
+        page_orientation: "portrait",
         header_html: undefined,
-        footer_html : '<p>1</p>',
-        header_and_footer_css : 'p {color: #333; text-align: right; font-size: 12px; padding-bottom: 50px; padding-right: 50px; } '
-      }
+        footer_html: "<p>1</p>",
+        header_and_footer_css:
+          "p {color: #333; text-align: right; font-size: 12px; padding-bottom: 50px; padding-right: 50px; } ",
+      },
     },
     pagination: {
       pageWidth: "21cm",
@@ -401,7 +402,8 @@ const config = computed(() => {
         "|",
       ],
     },
-    initialData: '<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n\tYou\'ve successfully created a CKEditor 5 project. This powerful text editor\n\twill enhance your application, enabling rich text editing capabilities that\n\tare customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n\t<li>\n\t\t<strong>Integrate into your app</strong>: time to bring the editing into\n\t\tyour application. Take the code you created and add to your application.\n\t</li>\n\t<li>\n\t\t<strong>Explore features:</strong> Experiment with different plugins and\n\t\ttoolbar options to discover what works best for your needs.\n\t</li>\n\t<li>\n\t\t<strong>Customize your editor:</strong> Tailor the editor\'s\n\t\tconfiguration to match your application\'s style and requirements. Or\n\t\teven write your plugin!\n\t</li>\n</ol>\n<p>\n\tKeep experimenting, and don\'t hesitate to push the boundaries of what you\n\tcan achieve with CKEditor 5. Your feedback is invaluable to us as we strive\n\tto improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n\t<li>📝 <a href="https://portal.ckeditor.com/checkout?plan=free">Trial sign up</a>,</li>\n\t<li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n\t<li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n\t<li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n\t<li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n\tSee this text, but the editor is not starting up? Check the browser\'s\n\tconsole for clues and guidance. It may be related to an incorrect license\n\tkey if you use premium features or another feature-related requirement. If\n\tyou cannot make it work, file a GitHub issue, and we will help as soon as\n\tpossible!\n</p>\n',
+    initialData:
+      '<h2>Congratulations on setting up CKEditor 5! 🎉</h2>\n<p>\n\tYou\'ve successfully created a CKEditor 5 project. This powerful text editor\n\twill enhance your application, enabling rich text editing capabilities that\n\tare customizable and easy to use.\n</p>\n<h3>What\'s next?</h3>\n<ol>\n\t<li>\n\t\t<strong>Integrate into your app</strong>: time to bring the editing into\n\t\tyour application. Take the code you created and add to your application.\n\t</li>\n\t<li>\n\t\t<strong>Explore features:</strong> Experiment with different plugins and\n\t\ttoolbar options to discover what works best for your needs.\n\t</li>\n\t<li>\n\t\t<strong>Customize your editor:</strong> Tailor the editor\'s\n\t\tconfiguration to match your application\'s style and requirements. Or\n\t\teven write your plugin!\n\t</li>\n</ol>\n<p>\n\tKeep experimenting, and don\'t hesitate to push the boundaries of what you\n\tcan achieve with CKEditor 5. Your feedback is invaluable to us as we strive\n\tto improve and evolve. Happy editing!\n</p>\n<h3>Helpful resources</h3>\n<ul>\n\t<li>📝 <a href="https://portal.ckeditor.com/checkout?plan=free">Trial sign up</a>,</li>\n\t<li>📕 <a href="https://ckeditor.com/docs/ckeditor5/latest/installation/index.html">Documentation</a>,</li>\n\t<li>⭐️ <a href="https://github.com/ckeditor/ckeditor5">GitHub</a> (star us if you can!),</li>\n\t<li>🏠 <a href="https://ckeditor.com">CKEditor Homepage</a>,</li>\n\t<li>🧑‍💻 <a href="https://ckeditor.com/ckeditor-5/demo/">CKEditor 5 Demos</a>,</li>\n</ul>\n<h3>Need help?</h3>\n<p>\n\tSee this text, but the editor is not starting up? Check the browser\'s\n\tconsole for clues and guidance. It may be related to an incorrect license\n\tkey if you use premium features or another feature-related requirement. If\n\tyou cannot make it work, file a GitHub issue, and we will help as soon as\n\tpossible!\n</p>\n',
     licenseKey: LICENSE_KEY,
     link: {
       addTargetToExternalLinks: true,
@@ -536,7 +538,7 @@ function onEditorReady(editor) {
 </script>
 
 <style>
-.editor-container{
+.editor-container {
   margin-top: 120px;
 }
 
@@ -549,4 +551,3 @@ function onEditorReady(editor) {
   padding-right: 40px;
 }
 </style>
-
